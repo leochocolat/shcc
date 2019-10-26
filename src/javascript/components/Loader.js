21b899213dd546b483f2c524a72440339d40e0c0
Sergio335566 <bocanceaserge335566@gmail.com>
2019-10-26 15:11:32 +0200

    add js components

---
+++ src/javascript/components/Loader.js
@@ -0,0 +1,10 @@
+import _ from 'underscore';
+class Loader {
+    constructor() {
+        this.container = document.querySelector('.js-container');
+    }
+    _setupFunctions() {
+
+    }
+}
+export default Loader;
