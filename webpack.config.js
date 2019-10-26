cae3a5a4c85223e84ac51c326604a59ca966b781
leochocolat <l.mouraire@outlook.fr>
2019-10-26 15:29:18 +0200

    remove copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const copyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,21 +7,7 @@ module.exports = {
     app: "./src/javascript/index.js"
   },
   plugins: [
-    new CleanWebpackPlugin(),
-    new copyPlugin([
-      {
-        from: "./src/html/**/*.html",
-        to: path.resolve(__dirname, "dist")
-      },
-      {
-        from: "./src/html/index.html",
-        to: path.resolve(__dirname, "dist")
-      },
-      {
-        from: "src/assets/",
-        to: path.resolve(__dirname, "dist", "assets")
-      }
-    ])
+    new CleanWebpackPlugin()
   ],
   devServer: {
     contentBase: "./dist"
@@ -51,14 +35,6 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ["file-loader"]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: ["csv-loader"]
-      },
-      {
-        test: /\.xml$/,
-        use: ["xml-loader"]
       }
     ]
   }
