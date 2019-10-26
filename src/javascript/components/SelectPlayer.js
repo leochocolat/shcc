8ee7596292766537e113f838f62c4365f6e76955
Sergio335566 <bocanceaserge335566@gmail.com>
2019-10-26 20:16:50 +0200

    deltaTime

--- src/javascript/components/SelectPlayer.js
+++ src/javascript/components/SelectPlayer.js
@@ -1,29 +1,32 @@
 import _ from 'underscore';
 import Pixi from './Pixi'
+import { TweenLite, Power3 } from 'gsap';
+
 class SelectPlayer {
     constructor() {
         // _.bindAll(this, '_selectPlayer');
         this.container = document.querySelector('.js-container');
-        this.firstPlayer = this.container.querySelector('.js-firstPlayer');
-        this.secondPlayer = this.container.querySelector('.js-secondPlayer');
+        this.playerContainer = this.container.querySelector('.js-playerContainer');
+        this.players = this.container.querySelectorAll('.js-player img');
         this.clicked = false;
         this._setup()
     }
-    _selectPlayer(playerType) {
+    _selectPlayer(index) {
         if (!this.clicked) {
             this.clicked = true;
-            // console.log(Pixi)
-            // new Pixi()
+            TweenLite.to(this.players[index], 1, { y: -20, ease: Power3.easeOut })
+            TweenLite.to(this.playerContainer, 0.5, { autoAlpha: 0, delay: 0.1 })
+            // this.playerContainer.classList.add('hidden');
+            // new Pixi(index)
         }
     }
     _setup() {
         this._setupEventListeners();
     }
     _setupEventListeners() {
-        const playerOne = 'bmx',
-            playerTwo = 'skate';
-        this.firstPlayer.addEventListener('click', this._selectPlayer.bind(this, playerOne));
-        this.secondPlayer.addEventListener('click', this._selectPlayer.bind(this, playerTwo));
+        for (let index = 0; index < this.players.length; index++) {
+            this.players[index].addEventListener('click', () => { this._selectPlayer(index) });
+        }
     }
 }
 export default SelectPlayer;
