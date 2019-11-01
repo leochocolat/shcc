d4dd8d6a5ae9e86deed5f5e5269e591f76e8fc39
leochocolat <l.mouraire@outlook.fr>
2019-11-01 16:04:54 +0100

    adjust positions buildings

--- src/javascript/components/Background.js
+++ src/javascript/components/Background.js
@@ -3,21 +3,21 @@ import { Z_ASCII } from "zlib";
 class Background {
 
     constructor(canvas, resources) {
-        this._canvas = canvas
-        this._resources = resources
+        this._canvas = canvas;
+        this._resources = resources;
+
         this._wallProperties = {
-            x: this._canvas.width + 500,
-            y: -500,
-            height: 450,
-            width: 350,
-            speed: { x: 6, y: 6 / 1.66 }
+            x: 0,
+            y: this._canvas.height - 200,
+            height: 850,
+            speed: 7
         }
 
-        // todo find perfect angle 
-        // this.x +=   this.speed * Math.cos(angle * Math.PI / 180);
-        // this.y -=   this.speed * Math.sin(angle * Math.PI / 180);
+        this.velocity = {};
+        this.velocity.x = this._wallProperties.speed * Math.cos(Math.PI * 30.75 / 180);
+        this.velocity.y = this._wallProperties.speed * Math.sin(Math.PI * 30.75 / 180);
 
-        this.setup()
+        this.setup();
     }
 
     setup() {
@@ -30,24 +30,29 @@ class Background {
         for (let i in this._resources.textures) {
             this._textures.push(this._resources.textures[i]);
         }
-
+        
         this._animatedObstacle = new PIXI.extras.AnimatedSprite(this._textures);
+        this._animatedObstacle.gotoAndStop(Math.round(Math.random() * this._textures.length));
+
+        let ratio = this._animatedObstacle.width / this._animatedObstacle.height;
+        this._wallProperties.width = this._wallProperties.height * ratio;
+
         this._animatedObstacle.width = this._wallProperties.width;
         this._animatedObstacle.height = this._wallProperties.height;
 
         this._animatedObstacle.position.x = this._wallProperties.x;
-        this._animatedObstacle.position.y = this._wallProperties.y;
+        this._animatedObstacle.position.y = this._wallProperties.y - this._wallProperties.height/2;
         
         this._obstaclesContainer.addChild(this._animatedObstacle);
     }
     
     updateBackgroundPosition() {
-        this._animatedObstacle.transform.position.x += - this._wallProperties.speed.x;
-        this._animatedObstacle.transform.position.y += this._wallProperties.speed.y;
+        this._animatedObstacle.transform.position.x += - this.velocity.x
+        this._animatedObstacle.transform.position.y += this.velocity.y;
 
         if (this._animatedObstacle.position.x + this._wallProperties.width < 0) {
             this._animatedObstacle.position.x = this._wallProperties.x;
-            this._animatedObstacle.position.y = this._wallProperties.y;
+            this._animatedObstacle.position.y = this._wallProperties.y - this._wallProperties.height/2;
             this._animatedObstacle.gotoAndStop(Math.round(Math.random() * this._textures.length));
         }
     }
