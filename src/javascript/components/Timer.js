67043ff89739d2fbf4a9f4a9e218f4e79b5e9791
Sergio335566 <bocanceaserge335566@gmail.com>
2019-11-11 14:44:39 +0100

    timer box css

--- src/javascript/components/Timer.js
+++ src/javascript/components/Timer.js
@@ -1,17 +1,5 @@
 class Timer {
-    constructor(stage) {
-        this._stage = stage;
-        const style = new PIXI.TextStyle();
-        this.timerText = new PIXI.Text('0', style);
-        style.dropShadow = true
-        style.dropShadowAlpha = 0.3
-        style.dropShadowAngle = Math.PI / 8
-        style.dropShadowDistance = 2
-        style.fill = 'white'
-        style.fontFamily = 'Krungthep'
-        this.timerText.position.set(window.innerWidth - 320, window.innerHeight - 250);
-        this.timerText.transform.skew.y = 0.125
-        this.timerText.scale.set(2.5, 2.5);
+    constructor() {
         this.initTimerValues();
     }
     initTimerValues() {
@@ -21,7 +9,7 @@ class Timer {
     getDeltaTime() {
         this.elapsedTime = Date.now() - this.startTime;
         this.seconds = (this.elapsedTime / 1000).toFixed(2)
-        this.timerText.text = `${this.seconds}s`
+        document.querySelector('.timer-value').innerHTML = `${this.seconds}s`
         return this.seconds
     }
     drawTimer() {
