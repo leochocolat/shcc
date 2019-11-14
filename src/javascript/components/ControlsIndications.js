d1619625c08f78abd4fe1a131c38d5d2e07490c2
leochocolat <l.mouraire@outlook.fr>
2019-11-14 13:40:29 +0100

    add didactitiel

--- src/javascript/components/ControlsIndications.js
+++ src/javascript/components/ControlsIndications.js
@@ -1,6 +1,5 @@
 import _ from 'underscore';
-import { TimelineLite, TweenLite, Power1 } from 'gsap';
-import checkIcon from '../../check-icon.png';
+import { TimelineLite, TweenLite, Power1, Power3, TweenMax } from 'gsap';
 
 class ControlsIndications {
     constructor(el) {
@@ -8,17 +7,93 @@ class ControlsIndications {
 
         this.el = el;
 
-        this.ui = { 
+        this.ui = {
+            title: this.el.querySelector('.js-title'),
             spacebar: this.el.querySelector('.js-key-spacebar'),
             left: this.el.querySelector('.js-key-left'),
             right: this.el.querySelector('.js-key-right'),
+            contentContainer: this.el.querySelector('.js-content-container'),
+            content: this.el.querySelector('.js-content')
         }
 
         this._setup();
     }
 
     _setup() {
+        this._setupTransitionTween();
+
+        TweenLite.set(this.ui.left, { autoAlpha: 0 });
+        TweenLite.set(this.ui.right, { autoAlpha: 0 });
+        TweenLite.set(this.ui.spacebar, { autoAlpha: 0 });
+
+        setTimeout(() => {
+            this.transitionIn();
+
+            setTimeout(() => {
+                this.transitionInKey(this.ui.spacebar);
+                setTimeout(() => {
+                    this.transitionOutKey(this.ui.spacebar);
+                }, 1000)
+            }, 1000)
+            setTimeout(() => {
+                this.transitionInKey(this.ui.left);
+                this.transitionInKey(this.ui.right);
+                setTimeout(() => {
+                    // this.transitionOutKey(this.ui.left);
+                    // this.transitionOutKey(this.ui.right);
+                }, 1000)
+            }, 2000)
+            setTimeout(() => {
+                setTimeout(() => {
+                    this.transitionOut();
+                }, 1000)
+            }, 3000)
+        }, 2000);
+
         this._setupEventListeners();
+    }
+
+    _setupTransitionTween() {
+        const TRANSLATE = 1000;
+        const INTERVAL = 0.1;
+        const DURATION = 1;
+
+        this.timelineIn = new TimelineLite({ paused: true });
+
+        this.timelineIn.fromTo(this.ui.contentContainer, DURATION, { height: 0 }, { height: '100%', ease: Power3.easeInOut }, INTERVAL * 0);
+        this.timelineIn.fromTo(this.ui.title, DURATION, { y: TRANSLATE }, { y: 0, ease: Power3.easeInOut }, INTERVAL * 1);
+
+        this.timelineOut = new TimelineLite({ paused: true });
+
+        this.timelineOut.fromTo(this.ui.title, DURATION, { y: 0 }, { y: -TRANSLATE, ease: Power3.easeInOut }, INTERVAL * 0);
+        this.timelineOut.fromTo(this.ui.contentContainer, DURATION, { height: '100%' }, { height: 0, ease: Power3.easeInOut }, INTERVAL * 4);
+    }
+
+    transitionIn() {
+        this.timelineIn.play();
+    }
+
+    transitionOut() {
+        this.timelineOut.play();
+    }
+
+    transitionInKey(el) {
+        const TRANSLATE = 1000;
+        const DURATION = 1;
+
+        TweenLite.fromTo(el, DURATION, { y: TRANSLATE }, { y: 0, ease: Power3.easeInOut });
+        TweenLite.set(el, { autoAlpha: 1 });
+    }
+
+    transitionOutKey(el) {
+        const TRANSLATE = 1000;
+        const DURATION = 1;
+
+        TweenLite.fromTo(el, DURATION, { y: 0 }, { y: -TRANSLATE, ease: Power3.easeInOut, onUpdate: () => {
+            if (el.getBoundingClientRect().y < 0) {
+                TweenLite.set(el, { autoAlpha: 0 });
+            }
+        } });
     }
 
     _playTween(el) {
