a023747d541356a8c04b1956068bd9ebb9d35956
leochocolat <l.mouraire@outlook.fr>
2020-06-28 19:15:13 +0200

    fix transition select player

--- src/javascript/components/SelectPlayer.js
+++ src/javascript/components/SelectPlayer.js
@@ -1,6 +1,6 @@
 import _ from 'underscore';
 import TextureLoader from '../modules/TextureLoader'
-import { TweenLite } from 'gsap';
+import { TweenLite, TimelineLite, Power4 } from 'gsap';
 
 class SelectPlayer {
     constructor() {
@@ -11,6 +11,8 @@ class SelectPlayer {
         this._ui = {
             playerContainer: this.container.querySelector('.js-playerContainer'),
             backgroundTransitionContainer: document.querySelector('.js-bgContainer'),
+            backgroundTransitionRed: document.querySelector('.js-bgContainer-red'),
+            backgroundTransitionWhite: document.querySelector('.js-bgContainer-white'),
             players: this.container.querySelectorAll('.js-player'),
             playerTimerHead: document.querySelector('.js-player-head')
         }
@@ -19,41 +21,48 @@ class SelectPlayer {
         this._setup();
     }
 
+    _setup() {
+        this._initStyle();
+        this._setupEventListeners();
+    }
+
+    _initStyle() {
+        let width = window.innerWidth;
+        let height = window.innerHeight;
+        let hyp = Math.sqrt(width * width + height * height);
+
+        this._ui.backgroundTransitionWhite.style.width = `${hyp}px`;
+        this._ui.backgroundTransitionWhite.style.height = `${hyp}px`;
+        this._ui.backgroundTransitionRed.style.width = `${hyp}px`;
+        this._ui.backgroundTransitionRed.style.height = `${hyp}px`;
+    }
+
     _selectPlayer(index) {
         if (!this.clicked) {
             this.clicked = true;
             this._textureLoader.loadedPlayerTexture(index);
-            let playerHead = this._ui.players[index].querySelector(".player__head")
-            TweenLite.to(playerHead, 0.5, { y: 2 });
+            let playerHead = this._ui.players[index].querySelector(".player__head");
+            this._ui.playerTimerHead.src = `assets/images/player${index + 1}-head-progressBar.png`;
 
-            this._ui.playerTimerHead.src = `assets/images/player${index + 1}-head-progressBar.png`
-            TweenLite.to(this._ui.playerContainer, 0.2, { autoAlpha: 0, delay: 2 });
-            TweenLite.to(this._ui.backgroundTransitionContainer, 1.5, { right: window.innerWidth * 4, top: '50%', delay: 1.5 });
+            let timeline = new TimelineLite({
+                onComplete: () => {
+                    this._ui.backgroundTransitionContainer.remove();
+                }
+            });
+            // timeline.timeScale(0.2);
+
+            timeline.set(this._ui.playerContainer, { autoAlpha: 0 }, 0.8);
+            timeline.set(this._ui.backgroundTransitionContainer, { autoAlpha: 1 }, 0);
+            timeline.to(this._ui.backgroundTransitionWhite, 1.6, { x: '-200%', ease: Power4.easeInOut }, 0);
+            timeline.to(this._ui.backgroundTransitionRed, 1.6, { x: '-200%', ease: Power4.easeInOut }, 0.1);
         }
-    }
-
-    _setup() {
-        this._setupEventListeners();
-    }
-
-    _selectPlayer(index) {
-        if (this.clicked) return;
-        this.clicked = true;
-
-        this._textureLoader.loadedPlayerTexture(index);
-
-        let playerHead = this._ui.players[index].querySelector(".player__head")
-        TweenLite.to(playerHead, 0.5, { y: 2 });
-
-        this._ui.playerTimerHead.src = `assets/images/player${index + 1}-head-progressBar.png`;
-        TweenLite.to(this._ui.playerContainer, 0.2, { autoAlpha: 0, delay: 2 });
-        TweenLite.to(this._ui.backgroundTransitionContainer, 1.5, { right: window.innerWidth * 4, top: '50%', delay: 1.5 });
     }
 
     _setupEventListeners() {
         for (let index = 0; index < this._ui.players.length; index++) {
             this._ui.players[index].addEventListener('click', () => { this._selectPlayer(index) });
         }
+        window.addEventListener('resize', () => { this._initStyle() });
     }
 }
 export default SelectPlayer;
