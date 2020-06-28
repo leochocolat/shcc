53e77afe028066a3dd08a84285840464b0627950
leochocolat <l.mouraire@outlook.fr>
2020-06-28 15:09:59 +0200

    add orientation screen message

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,50 @@
 import SelectPlayer from './components/SelectPlayer';
 
+let width, height;
+let orientationMessage = document.querySelector('.js-orientation-message');
+
 document.addEventListener('DOMContentLoaded', () => {
-    new SelectPlayer()
+    setup();
 });
+
+function setup() {
+    new SelectPlayer();
+    checkSize();
+    setupEventListeners();
+}
+
+function checkSize() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    if (width < height) {
+        displayOrientationMessage();
+    } else {
+        removeOrientationMessage();
+    }
+}
+
+function displayOrientationMessage() {
+    if (!orientationMessage.classList.contains('isDisable')) return;
+
+    orientationMessage.classList.remove('isDisable');
+}
+
+function removeOrientationMessage() {
+    if (orientationMessage.classList.contains('isDisable')) return;
+    
+    orientationMessage.classList.add('isDisable');
+}
+
+function setupEventListeners() {
+    window.addEventListener('orientationchange', orientationchangeHandler);
+}
+
+function orientationchangeHandler(e) {
+    window.addEventListener('resize', resizeOrientationHandler);
+}
+
+function resizeOrientationHandler() {
+    checkSize();
+    window.removeEventListener('resize', resizeOrientationHandler);
+}
